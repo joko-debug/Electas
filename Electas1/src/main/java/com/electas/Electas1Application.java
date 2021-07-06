a4d79f8b24cb68257b741be9d3616f96812aea30
User <User@DESKTOP-FJTUE3B>
2021-07-06 13:08:52 +0200

    first commit

---
+++ Electas1/src/main/java/com/electas/Electas1Application.java
@@ -0,0 +1,13 @@
+package com.electas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Electas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Electas1Application.class, args);
+	}
+
+}
---
+++ Electas1/src/test/java/com/electas/Electas1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Electas1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
