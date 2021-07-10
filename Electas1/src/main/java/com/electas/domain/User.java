318c144f5cd2a713ed67ce401b8b20d888ea546d
DESKTOP-FJTUE3B\User <njokozela@outlook.com>
2021-07-10 10:35:16 +0200

    working  controller and place holder landing page

--- Electas1/src/main/java/com/electas/domain/User.java
+++ Electas1/src/main/java/com/electas/domain/User.java
@@ -17,105 +17,126 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @Entity
 @Table(name = "users")
-@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id")
 public class User {
 
-	 private Long id;
-	  private String username;
-	  private String password;
-	  private String firstName;
-	  private String lastName;
-	  private String email;
-	  private String description;
-	  private String idNumber;
-	  private Set<Authority> authorities = new HashSet<>();
-	  private Set<Candidate> candidates = new HashSet<>();
-	  private Set<Election> elections = new HashSet<>();
-	  private Set<Ballot> ballots = new HashSet<>();
-	  private Set<Sment> sments = new HashSet<>();
-	  private Set<UpDown> updown = new HashSet<>();
-	  
-	  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String description;
+	private String idNumber;
+	private Set<Authority> authorities = new HashSet<>();
+	private Set<Candidate> candidates = new HashSet<>();
+	private Set<Election> elections = new HashSet<>();
+	private Set<Ballot> ballots = new HashSet<>();
+	private Set<Sment> sments = new HashSet<>();
+	private Set<UpDown> updown = new HashSet<>();
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getIdNumber() {
 		return idNumber;
 	}
+
 	public void setIdNumber(String idNumber) {
 		this.idNumber = idNumber;
 	}
-	
-	@OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY, mappedBy="user")
+
+	@OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "user")
 	public Set<Authority> getAuthorities() {
 		return authorities;
 	}
+
 	public void setAuthorities(Set<Authority> authorities) {
 		this.authorities = authorities;
 	}
-	
-	@OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY, mappedBy="user")
+
+	@OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "user")
 	public Set<Candidate> getCandidates() {
 		return candidates;
 	}
+
 	public void setCandidates(Set<Candidate> candidates) {
 		this.candidates = candidates;
 	}
-	
-	@OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY, mappedBy="admin")
+
+	@OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "admin")
 	public Set<Election> getElections() {
 		return elections;
 	}
+
 	public void setElections(Set<Election> elections) {
 		this.elections = elections;
 	}
-	
-	@OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY, mappedBy="user")
+
+	@OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "user")
 	public Set<Ballot> getBallots() {
 		return ballots;
 	}
+
 	public void setBallots(Set<Ballot> ballots) {
 		this.ballots = ballots;
 	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", password=" + password + ", firstName=" + firstName
@@ -123,33 +144,35 @@ public class User {
 				+ idNumber + ", authorities=" + authorities + ", candidates=" + candidates + ", elections=" + elections
 				+ ", ballots=" + ballots + "]";
 	}
+
 	/**
 	 * @return the sments
 	 */
-	@OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY, mappedBy="user")
+	@OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "user")
 	public Set<Sment> getSments() {
 		return sments;
 	}
+
 	/**
 	 * @param sments the sments to set
 	 */
 	public void setSments(Set<Sment> sments) {
 		this.sments = sments;
 	}
+
 	/**
 	 * @return the updown
 	 */
-	@OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY, mappedBy="user")
+	@OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "user")
 	public Set<UpDown> getUpdown() {
 		return updown;
 	}
+
 	/**
 	 * @param updown the updown to set
 	 */
 	public void setUpdown(Set<UpDown> updown) {
 		this.updown = updown;
 	}
-	  
-	  
-	
+
 }
---
+++ Electas1/src/main/java/com/electas/sercurity/WebSecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.electas.sercurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+
+//configures web security for every and also sets permitions for each
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	  private UserDetailsService userDetailsService;
+
+	  @Bean
+	  public PasswordEncoder getPasswordEncoder() {
+	    return new BCryptPasswordEncoder();
+	  }
+	  
+	  @Override
+	  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	    auth
+	      .userDetailsService(userDetailsService)
+	      .passwordEncoder(getPasswordEncoder());
+	        
+	  }
+	  
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http
+	        .authorizeRequests()
+	          .antMatchers("/").permitAll()
+	          .antMatchers("/admin/**").hasRole("ADMIN")
+	          .anyRequest().hasRole("USER").and()
+	        .formLogin()
+	          .loginPage("/landing_page")
+	          .defaultSuccessUrl("/dashboard")
+	          .permitAll()
+	          .and()
+	        .logout()
+	          .logoutUrl("/logout")
+	          .permitAll();
+	  }
+	
+}
