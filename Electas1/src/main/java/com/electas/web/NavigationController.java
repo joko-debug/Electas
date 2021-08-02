86685a7cb48d5c92dab330fffd7900e4b5bbb957
DESKTOP-FJTUE3B\User <njokozela@outlook.com>
2021-08-02 19:07:32 +0200

    working navigation, pages not complete, and theme/ inas pages not complete

--- Electas1/src/main/java/com/electas/web/NavigationController.java
+++ Electas1/src/main/java/com/electas/web/NavigationController.java
@@ -1,24 +1,41 @@
 package com.electas.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.electas.domain.User;
 
+
 @Controller
 public class NavigationController {
 
 	@GetMapping("/nav")
-	public String homeView(User user, ModelMap model) {
+	public String homeView(@AuthenticationPrincipal User user, ModelMap model) {
 		return "nav";
 	}
 	
 	
 	 @GetMapping("/nav/home")
-	    public String getContent1() {
+	    public String navhome() {
 		 //ModelAndView mv= new ModelAndView("search::search_list"); 
 	        return "fragments/navigation::home";
+	    }
+	 @GetMapping("/nav/election")
+	    public String navelection() {
+		 //ModelAndView mv= new ModelAndView("search::search_list"); 
+	        return "fragments/navigation::election";
+	    }
+	 @GetMapping("/nav/profile")
+	    public String navprofile() {
+		 //ModelAndView mv= new ModelAndView("search::search_list"); 
+	        return "fragments/navigation::profile";
+	    }
+	 @GetMapping("/nav/help")
+	    public String navhelp() {
+		 //ModelAndView mv= new ModelAndView("search::search_list"); 
+	        return "fragments/navigation::help";
 	    }
 
 	    @RequestMapping("/content2")
